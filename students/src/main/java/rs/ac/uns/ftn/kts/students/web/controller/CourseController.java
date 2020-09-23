26e7f35bcd15ace1d496f8479c0b4c1acb5bd097
Nenad <nenad.ralic14@gmail.com>
2020-09-23 10:22:31 +0200

    Dodat CrossOrigin u CourseController-u.

--- students/src/main/java/rs/ac/uns/ftn/kts/students/web/controller/CourseController.java
+++ students/src/main/java/rs/ac/uns/ftn/kts/students/web/controller/CourseController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import rs.ac.uns.ftn.kts.students.web.dto.StudentDTO;
 
 @RestController
 @RequestMapping(value="api/courses")
+@CrossOrigin(origins="http://localhost:4200",allowedHeaders="*")
 public class CourseController {
 	@Autowired
 	private CourseService courseService;
